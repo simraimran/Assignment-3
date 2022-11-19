aadf624b6b5cd42d16c041de835dc071f569aee3
simraimran <113390014+simraimran@users.noreply.github.com>
2022-11-19 00:16:22 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+/*client side*/
+// IIFE--> immediately invoked function expression*
+(function(){
+    function Start()
+    {
+        console.log("App Started");
+        
+    }
+    window.addEventListener("load",Start);
+
+})();
